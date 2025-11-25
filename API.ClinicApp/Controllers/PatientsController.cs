e9aaab22ace1152ed20d8f512920591987427731
Jaylon Coad <jayloncoad@gmail.com>
2025-11-25 02:50:52 -0500

    Completed PatientsController for the API

--- API.ClinicApp/Controllers/PatientsController.cs
+++ API.ClinicApp/Controllers/PatientsController.cs
@@ -16,15 +16,16 @@ public class PatientsController : ControllerBase
         _context = context;
     }
 
-    // GET: /Patients
+    // GET: returns all patients from the database
+    // Endpoint: /Patients
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Patient>>> Get()
+    public async Task<ActionResult<IEnumerable<Patient>>> Get() // returning an IEnumerable<T> because this is read-only and doesn't lock us into using a List<T> data structure if we wanted to change data types later since they are all technically IEnumerable<T>
     {
         return await _context.Patients.ToListAsync();
     }
 
-    // GET: /Patients/{id}
-    // Example: /Patients/X99Z12
+    // GET: returns one patient given an id
+    // Endpoint: /Patients/{id}
     [HttpGet("{id}")]
     public async Task<ActionResult<Patient>> GetById(string id)
     {
@@ -36,17 +37,60 @@ public class PatientsController : ControllerBase
         return patient;
     }
 
-    // POST: /Patients
+    // POST: adds a patient to the database
+    // Endpoint: /Patients
     [HttpPost]
-    public async Task<ActionResult<Patient>> Add([FromBody] Patient patient)
+    public async Task<ActionResult<Patient>> Add(Patient patient)
     {
-        // We rely on the MAUI app (Client) to generate the String ID
         if (string.IsNullOrEmpty(patient.Id))
         {
-            return BadRequest("Patient ID must be provided by the client.");
+            return BadRequest("Patient ID must be provided by the client."); // bad request means there was a problem with the data from the client side
         }
         _context.Patients.Add(patient);
         await _context.SaveChangesAsync();
         return CreatedAtAction(nameof(GetById), new { id = patient.Id }, patient);
+    }
+
+    // Delete: deletes a patient from the database given an id
+    // Endpoint: /Patients/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var patient = await _context.Patients.FindAsync(id);
+        if (patient == null)
+        {
+            return NotFound();
+        }
+        _context.Patients.Remove(patient);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    // Update: updates a patient from the database given an id that already exists
+    // Endpoint: /Patients/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Patient patient, string id)
+    {
+        if (patient.Id != id)
+        {
+            return BadRequest("Patient ID is not equal to the incoming ID.");
+        }
+        _context.Entry(patient).State = EntityState.Modified; // this line basically tells the server that we are trying to overwrite the existing object
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch(DbUpdateConcurrencyException)
+        {
+            if (!_context.Patients.Any(p => p.Id == id)) // couldn't find the existing object in the database
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return NoContent();
     }
 }
